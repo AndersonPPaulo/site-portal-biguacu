bba58bb90ea62fa5e606a91a131773c1993f0e13
kayrongradilone <kayrongradilone@hotmail.com>
2026-03-09 17:33:12 -0300

    fix: update margin for CompanyGridSection component on non-article pages

--- src/components/companys/company-grid-section.tsx
+++ src/components/companys/company-grid-section.tsx
@@ -176,7 +176,7 @@ export function CompanyGridSection() {
   return (
     <section
       className={`w-full max-w-[1272px] mx-auto px-4 ${
-        !isArticlePage ? "mt-8 md:mt-10 py-2" : ""
+        !isArticlePage ? "mt-8 md:mt-15 py-2" : ""
       }`}
     >
       <div className="w-[106px] h-2 bg-red-500 rounded-full" />

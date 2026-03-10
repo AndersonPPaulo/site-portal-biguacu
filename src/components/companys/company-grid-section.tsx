8d2534f44b2b6dd2ee96ad5625bd2d973c24156c
kayrongradilone <kayrongradilone@hotmail.com>
2026-03-09 21:48:42 -0300

    fix: update margin for CompanyGridSection and CompanyGridSectionWithSkeleton components

--- src/components/companys/company-grid-section.tsx
+++ src/components/companys/company-grid-section.tsx
@@ -176,19 +176,19 @@ export function CompanyGridSection() {
   return (
     <section
       className={`w-full max-w-[1272px] mx-auto px-4 ${
-        !isArticlePage ? "mt-8 md:mt-15 py-2" : ""
+        !isArticlePage ? "mt-8 md:mt-16 py-2" : ""
       }`}
     >
       <div className="w-[106px] h-2 bg-red-500 rounded-full" />
 
-      <div className="flex flex-col md:flex-row items-start gap-3 md:items-center justify-between">
+      <div className="flex flex-col md:flex-row items-start md:items-center justify-between">
         <h2 className="text-2xl font-semibold text-red-500 py-6">
           Comércios que são destaques em Biguaçu!
         </h2>
 
         <Link
           href="/comercio"
-          className="flex items-center mb-3 font-semibold gap-2 text-zinc-900 hover:text-red-500 transition-colors group"
+          className="hidden md:flex items-center font-semibold gap-2 text-zinc-900 hover:text-red-500 transition-colors group"
         >
           Veja mais
           <ArrowRight
@@ -245,7 +245,7 @@ export function CompanyGridSectionWithSkeleton() {
 
   if (error) {
     return (
-      <section className="w-full mt-8 md:mt-32 py-12 max-w-[1272px] mx-auto px-4">
+      <section className="w-full mt-8 md:mt-16 py-12 max-w-[1272px] mx-auto px-4">
         <div className="text-center py-20">
           <p className="text-gray-600 mb-4">
             Não foi possível carregar os destaques.
@@ -266,17 +266,17 @@ export function CompanyGridSectionWithSkeleton() {
   }
 
   return (
-    <section className="w-full mt-8 md:mt-32 py-12 max-w-[1272px] mx-auto px-4">
+    <section className="w-full mt-8 md:mt-16 py-12 max-w-[1272px] mx-auto px-4">
       <div className="w-[106px] h-2 bg-red-500 rounded-full" />
 
-      <div className="flex flex-col md:flex-row items-start gap-3 md:items-center justify-between">
+      <div className="flex flex-col md:flex-row items-start md:items-center justify-between">
         <h2 className="text-2xl font-semibold text-red-500 py-6">
           Comércios que são destaques em Biguaçu!
         </h2>
 
         <Link
           href="/comercio"
-          className="flex items-center font-semibold gap-2 text-zinc-900 hover:text-red-500 transition-colors group"
+          className="hidden md:flex items-center font-semibold gap-2 text-zinc-900 hover:text-red-500 transition-colors group"
         >
           Veja mais
           <ArrowRight
